87e317a50184d2633958f4467b7fe9bfff02df84
Ali Kayli <kayliali224@gmail.com>
2026-01-27 12:16:52 +0300

    Fixed in Bubble Sort

--- Bubble_sort1.py
+++ Bubble_sort1.py
@@ -1,13 +1,17 @@
 #bubble sort
 
+def bubble_sort(liste):
+    kontrol=False
+    while kontrol==False:
+        kontrol=True
+        for i in range(len(liste)-1):
+            if liste[i]>liste[i+1]:
+                kontrol=False
+                liste[i],liste[i+1]=liste[i+1],liste[i]
+
+    return liste
+
 
 liste=[3,8,5,7,10,9,1,0,-2]
-kontrol=False
-while kontrol==False:
-    kontrol=True
-    for i in range(len(liste)-1):
-        if liste[i]>liste[i+1]:
-            kontrol=False
-            liste[i],liste[i+1]=liste[i+1],liste[i]
-
-print(liste)
+sirali=bubble_sort(liste)
+print(sirali)
